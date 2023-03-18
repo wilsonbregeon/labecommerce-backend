cde52caaa869b20fc69311a10dc4bd67f72701b0
Wilson Bregeon <93269617+wilsonbregeon@users.noreply.github.com>
2023-03-18 18:09:53 -0300

    Delete build directory

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const database_1 = require("./database");
-console.log((0, database_1.getAllPurchasesFromClientId)("1"));
-//# sourceMappingURL=index.js.map
