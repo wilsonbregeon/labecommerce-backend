8aa3b1a8cd05837cc6ac14fbf3bcabd76dde2740
wilsonbregeon <wilsonbregeon@gmail.com>
2023-03-18 18:18:31 -0300

    relacoes SQL I

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -12,6 +12,9 @@ VALUES
 ("u002", "ciclano@hotmail.com", "ciclano1990"),
 ("u003", "beltrano@gmail.com", "beltrano2000");
 
+INSERT INTO users (id, email, password)
+VALUES ("u004", "astrodev@email.com", "astrodev123");
+
 SELECT * FROM users;
 
 
@@ -30,4 +33,68 @@ VALUES
 ("p004", "placa de vídeo", 3000, "hardware"),
 ("p005", "processador ryzen 7", 2500, "hardware");
 
+INSERT INTO products (id, name, price, category)
+VALUES ("p006", "web cam", 150, "periférico");
+
 SELECT * FROM products;
+
+SELECT * FROM products
+WHERE name = "mouse";
+
+SELECT * FROM products
+WHERE id = "p005";
+
+DELETE FROM users
+WHERE id = "u004";
+
+DELETE FROM products
+WHERE id = "p006";
+
+UPDATE users 
+SET email = "astrodev99@email.com"
+WHERE id = "u004";
+
+UPDATE products
+SET name = "cooler"
+WHERE id = "p006";
+
+SELECT * FROM users
+ORDER BY email ASC;
+
+SELECT * FROM products
+ORDER BY price ASC
+LIMIT 10;
+
+SELECT * FROM products
+WHERE price >= 1000 AND price <= 2500
+ORDER BY price ASC;
+
+CREATE TABLE purchases (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    total_price REAL NOT NULL,
+    paid INTEGER NOT NULL,
+    delivered_at TEXT NULL,
+    buyer_id TEXT NOT NULL,
+    FOREIGN KEY (buyer_id) REFERENCES users (id)
+);
+
+INSERT INTO purchases (id, total_price, paid, delivered_at, buyer_id)
+VALUES
+("purchase001", 1000, 0, NULL, "u001"),
+("purchase002", 2500, 0, NULL, "u001"),
+("purchase003", 1200, 0, NULL, "u002"),
+("purchase004", 1800, 0, NULL, "u002");
+
+SELECT * FROM purchases;
+
+UPDATE purchases
+SET delivered_at = "18/03/2023"
+WHERE buyer_id = "u001";
+
+UPDATE purchases
+SET delivered_at = "20/03/2023"
+WHERE buyer_id = "u002";
+
+SELECT * FROM purchases
+INNER JOIN users
+ON purchases.buyer_id = users.id;
